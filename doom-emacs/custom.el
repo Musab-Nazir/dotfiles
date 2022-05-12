262ed24c3609e06dfdd487e89260e6cfb18f3063
Musab <musabnazir@gmail.com>
2022-05-12 11:31:22 -0400

    Added .doom.d directory

---
+++ doom-emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8ad4ffc916d053e09c0b63ae37f1dee0c0573349bc91a9f2984f95970ac48d0d" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" default))
+ '(package-selected-packages '(janet-mode autothemer dash)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
