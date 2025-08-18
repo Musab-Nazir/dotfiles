1e865af79c7cc63b69ab13dcf07f30dae59101af
Musab <musabnazir@gmail.com>
2025-08-17 20:02:36 -0400

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,13 @@ end
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# evil helix
+set -gx PATH ~/evil-helix-amd64-linux/helix $PATH
+
+# roc
+set -gx PATH ~/roc-alpha3-rolling $PATH
+
 # aliases
-alias ls="eza"
+# alias ls="eza"
 alias cat="bat"
 alias top="btop"
