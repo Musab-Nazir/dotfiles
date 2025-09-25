a4a8a45d68d6950f0ee072572431dc68f090eb42
Musab <musabnazir@gmail.com>
2025-09-25 17:00:58 -0400

    proton stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,11 @@ set -gx PATH ~/helix $PATH
 # roc
 set -gx PATH ~/roc-alpha3-rolling $PATH
 
+# proton
+set -Ux PROTON_USE_NTSYNC 1
+set -Ux PROTON_ENABLE_WAYLAND 1
+set -Ux PROTON_FSR4_UPGRADE 1
+
 # aliases
 # alias ls="eza"
 alias cat="bat"
